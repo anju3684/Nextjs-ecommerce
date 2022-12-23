c54840c2e4834d741819a51ceede78ef4c7c47c5
Anjali Shah <57403861+anju3684@users.noreply.github.com>
2022-12-23 09:57:25 +0530

    Update next.config.js

--- next.config.js
+++ next.config.js
@@ -11,7 +11,7 @@ const nextConfig = {
 module.exports = {
   nextConfig,
   env: {
-    BASE_URL: "https://nextjs-ecommerce-anju3684.vercel.app",
+    BASE_URL: "https://nextjs-ecommerce1.herokuapp.com/",
     MONGODB_URL:
       "mongodb+srv://anju3684:anju3684@cluster0.8qqva.mongodb.net/nextjs-ecommerce?retryWrites=true&w=majority",
 
